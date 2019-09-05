f59a85c2533d96edd0fea715b52e53b225b17722
Takeshi Ikuma <tikuma@gmail.com>
2019-09-05 12:18:24 -0500

    Added ffmpeg_duration_t & get_pts()

--- src/ffmpegTimeUtil.h
+++ src/ffmpegTimeUtil.h
@@ -3,12 +3,16 @@
 #include <chrono>
 extern "C"
 {
+#include <libavutil/avutil.h>
 #include <libavutil/mathematics.h>
 #include <libavutil/rational.h>
 }
 
 namespace ffmpeg
 {
+
+typedef std::chrono::duration<int64_t, std::ratio<1, AV_TIME_BASE>>
+    ffmpeg_duration_t;
 
 template <typename Chrono_t>
 Chrono_t get_timestamp(uint64_t ts, const AVRational &tb)
@@ -22,6 +26,21 @@ Chrono_t get_timestamp(uint64_t ts, const AVRational &tb)
   {
     return Chrono_t(
         av_rescale_q(ts, tb, {Chrono_t::period::num, Chrono_t::period::den}));
+  }
+}
+
+template <typename Chrono_t> int64_t get_pts(Chrono_t t, const AVRational &tb)
+{
+  if constexpr (std::is_floating_point_v<typename Chrono_t::rep>)
+  {
+    using dbl_duration_t = std::chrono::duration<double>;
+    dbl_duration_t T = std::chrono::duration_cast<dbl_duration_t>(t);
+    return (int64_t)std::round(T.count() / av_q2d(tb));
+  }
+  else
+  {
+    return av_rescale_q(t.count(),
+                        {Chrono_t::period::num, Chrono_t::period::den}, tb);
   }
 }
 
